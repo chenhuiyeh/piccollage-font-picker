008e11a80410a84ffaaf752ccf5c44f9b54b37cb
Tiffany Yeh <33907866+chenhuiyeh@users.noreply.github.com>
2019-02-25 19:32:14 -0500

    Update Constants.java

--- app/src/main/java/com/piccollage/googlefontpicker/Constants.java
+++ app/src/main/java/com/piccollage/googlefontpicker/Constants.java
@@ -1,17 +1,5 @@
 package com.piccollage.googlefontpicker;
 
 public class Constants {
-    static String API_KEY = "AIzaSyCcPir6N7VMkxy0Wkk2P8Dlg59ub4sY8qg";
-
-    static final int WIDTH_DEFAULT = 100;
-    static final int WIDTH_MAX = 1000;
-    static final int WIDTH_MIN = 0;
-
-    static final int WEIGHT_DEFAULT = 400;
-    static final int WEIGHT_MAX = 1000;
-    static final int WEIGHT_MIN = 0;
-
-    static final float ITALIC_DEFAULT = 0f;
-    static final float ITALIC_MAX = 1f;
-    static final float ITALIC_MIN = 0f;
+    static String BASE_URL = "https://www.googleapis.com/webfonts/v1/webfonts/";
 }
