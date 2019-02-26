74ec4b9e9ff72ae8cf42c3ed2b29ba0fe0695cc6
Tiffany Yeh <33907866+chenhuiyeh@users.noreply.github.com>
2019-02-25 19:33:08 -0500

    Update WebFontClient.java

--- app/src/main/java/com/piccollage/googlefontpicker/WebFontClient.java
+++ app/src/main/java/com/piccollage/googlefontpicker/WebFontClient.java
@@ -3,10 +3,12 @@ package com.piccollage.googlefontpicker;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.piccollage.googlefontpicker.Constants.BASE_URL;
+
 public class WebFontClient {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://www.googleapis.com/webfonts/v1/webfonts/";
+    
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
